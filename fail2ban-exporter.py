0015397eea5f9b6531023896f2f80f59be323484
David Fischer <david@konst.fish>
2023-10-31 19:36:10 +0100

    feat(exporter): Update exporter to make wsgi app

--- fail2ban-exporter.py
+++ fail2ban-exporter.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 import re, subprocess, argparse
-from prometheus_client import CollectorRegistry, Counter, Gauge, generate_latest
-
-## Filename
-EXPORT_FILE_NAME="fail2ban.prom"
-
-## File output location for node_exporter
-EXPORT_LOCATION='/var/lib/prometheus/node-exporter'
+from prometheus_client import CollectorRegistry, Gauge, make_wsgi_app
+from prometheus_client.exposition import make_server
 
 ## Modifying may break it!
+## ^ hes right
 parseKeys = {
     'Currently failed:': ('fail2ban_failed_current', 'Number of currently failed connections.'),
     'Total failed:':('fail2ban_failed_total', 'Total number of failed connections.'),
@@ -17,15 +13,13 @@ parseKeys = {
 }
 
 ## Commandline args
-parser = argparse.ArgumentParser(description="Export fail2ban-client metrics for Prometheus Node Exporter.")
+parser = argparse.ArgumentParser(description="Export fail2ban-client metrics for Prometheus.")
 parser.add_argument('-j', '--jail', help="Jail name to be exported (all jails if omitted).")
-parser.add_argument('-f', '--file', help="File to write metrics to.")
+parser.add_argument('-p', '--port', type=int, default=8042, help="Port for the HTTP server.")
 args = parser.parse_args()
 
-## Regex
 pattern = re.compile(r'('+ '|'.join(parseKeys.keys()) + ')\s*(\d*)')
 metrics = {}
-output = ''
 
 for k in parseKeys.keys():
     metrics[k] = {}
@@ -38,19 +32,28 @@ else:
     match = re.search('.+Jail list:\s+(.+)$', response)
     jails = match.group(1).split(", ")
 
-for jail in jails:
-    process = subprocess.Popen(['fail2ban-client', 'status', jail], stdout=subprocess.PIPE)
-    response = process.communicate()[0].decode('utf-8')
-    match = re.findall(pattern, response)
-    for m in match:
-        metrics[m[0]].update( [(jail, float(m[1]))] )
-
 registry = CollectorRegistry()
-for (metric, jails) in metrics.items():
-    gauge = Gauge(parseKeys[metric][0], parseKeys[metric][1], ['jail'], registry=registry)
-    for (jail,value) in jails.items():
-        gauge.labels(jail).set(float(value))
 
-with open((args.file or (EXPORT_LOCATION + '/' + EXPORT_FILE_NAME)), "wb") as file:
-  file.write(generate_latest(registry))
-  file.close()
+for metric_key, metric_val in parseKeys.items():
+    metrics[metric_key] = Gauge(metric_val[0], metric_val[1], ['jail'], registry=registry)
+
+def update_metrics():
+    for jail in jails:
+        process = subprocess.Popen(['fail2ban-client', 'status', jail], stdout=subprocess.PIPE)
+        response = process.communicate()[0].decode('utf-8')
+        match = re.findall(pattern, response)
+        for m in match:
+            metrics[m[0]].labels(jail).set(float(m[1]))
+
+class CustomMetricsApp(object):
+    def __init__(self, app):
+        self._app = app
+
+    def __call__(self, environ, start_response):
+        update_metrics()
+        return self._app(environ, start_response)
+
+wrapped_app = CustomMetricsApp(make_wsgi_app(registry))
+
+httpd = make_server('', args.port, wrapped_app)
+httpd.serve_forever()
